708c152e8dc9e7987a3e7f3a14c2880e896237ac
Petro Korienev <soxjke@gmail.com>
2016-10-17 11:18:56 +0300

    Initial commit

--- WKWebViewTest/ViewController.swift
+++ WKWebViewTest/ViewController.swift
@@ -7,11 +7,27 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView = WKWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let views = [
+            "webView" : webView
+        ]
+        view.addSubview(webView)
+        var constraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[webView]|", options: [.AlignAllLeading, .AlignAllTrailing], metrics: nil, views: views)
+        constraints.appendContentsOf(NSLayoutConstraint.constraintsWithVisualFormat("V:|[webView]|", options: [.AlignAllTop, .AlignAllBottom], metrics: nil, views: views))
+        NSLayoutConstraint.activateConstraints(constraints)
+        
+        let path = NSBundle.mainBundle().pathForResource("ios - WKWebView fails to load images and CSS using loadHTMLString(_, baseURL_) - Stack Overflow", ofType: "htm")
+        let url = NSURL(fileURLWithPath: path!)
+        webView.loadHTMLString(try! String(contentsOfURL: url), baseURL: url.URLByDeletingLastPathComponent)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
